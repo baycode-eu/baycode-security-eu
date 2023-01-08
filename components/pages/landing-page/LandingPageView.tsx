a9b7ba79e574e406d2f66f892c14dc6388521849
Krystian Bajno <krystian.bajno@gmail.com>
2023-01-08 03:01:54 +0100

    commit

--- components/pages/landing-page/LandingPageView.tsx
+++ components/pages/landing-page/LandingPageView.tsx
@@ -24,7 +24,7 @@ const LandingPageView = (props) => {
 
     welcomeBaycode={
       <Card
-        title={<>>_Who are <span style={{color: "#0080e3ff"}}>we</span></>}
+        title={<>Be acknowledged of<span style={{color: "#0080e3ff"}}> threat</span></>}
         ><><p>Our mission is to help businesses and organizations secure and grow their systems, as well as protect their data from cyber threats. We offer a range of services to identify vulnerabilities, assess risk, and provide solutions for improving security.</p><br/>
           <p>Our team  has a deep understanding of the latest cybersecurity technologies and techniques, as well as extensive experience in software development.</p><br/>
           <p>We are committed to staying up-to-date with the latest industry trends and best practices. </p><br/>
@@ -54,7 +54,7 @@ const LandingPageView = (props) => {
 
     welcomeDevelopedApplicationsSecurity={
       <Card
-        title={"App Security Testing and Code Review"}
+        title={<>Code<span style={{color: "#0080e3ff"}}> Review</span></>}
       > <><p>We understand the importance of testing the security of developed applications and the role that code review plays in this process. In today's digital landscape, it is crucial for businesses and organizations to ensure that their software is secure and protected from potential cyber threats.</p><br/>
           <p>By testing the security of your developed applications and conducting thorough code reviews, you can ensure that your software is robust and resistant to potential threats. This not only protects your business and your customers, but it also helps to maintain trust and confidence in your products and services.</p><br/>
           <p>When integrating these practices into your workflow, you can identify and address vulnerabilities early on, resulting in a more secure and reliable final product.</p><br/>
@@ -64,13 +64,11 @@ const LandingPageView = (props) => {
 
     welcomeSoarSiem={
       <Card
-        title={"Baycode Security: Your Trusted Cybersecurity Partner"}
-       > <>
-            <p>We offer a range of cybersecurity services designed to help businesses and organizations protect their systems and data from cyber threats. One of the services we offer is SOAR (Security, Orchestration, Automation, and Response), which helps organizations automate and streamline their security processes.</p><br/>
-            <p>SOAR is a powerful tool that can help businesses and organizations improve their security posture and respond more effectively to cyber threats. By automating routine tasks and providing a centralized platform for managing security incidents, SOAR can save time and resources, allowing security teams to focus on more critical tasks.</p><br/>
-            <p>In addition to SOAR, we also offer SIEM (Security Information and Event Management) and threat hunting services. SIEM is a security tool that helps organizations monitor their systems for security events and alert them to potential threats. Threat hunting, on the other hand, involves proactively searching for and identifying potential threats within an organization's network.</p><br/>
-            <p>At Baycode Security, we also have expertise in cloud development and cloud security. We can help you design and develop cloud-based solutions that are secure and compliant with industry standards. Additionally, we are skilled in securing cloud environments, including those built on Microsoft Azure.</p><br/>
-            <p>Contact us today to learn more about our SOAR, SIEM, threat hunting, cloud development, and cloud security services and how they can help protect your business from cyber threats.</p>
+        title={<>A Trusted<span style={{color: "#0080e3ff"}}> Service</span></>}
+       ><>
+          <p>Baycode Security offers cybersecurity services including SOAR (Security, Orchestration, Automation, and Response), which is a powerful tool that helps organizations automate and streamline their security processes. SOAR can improve a business' security posture and allow to respond more effectively to cyber threats by automating routine tasks and providing a centralized platform for managing security incidents. This can save time and resources, allowing security teams to focus on more critical tasks.</p><br/>
+          <p>We also offer threat hunting services, which involves proactively searching for and identifying potential threats within an organization's network. </p><br/>
+          <p>We expertise in cloud development and cloud security and can deliever secure, compliant cloud-based solutions, including those built on Microsoft Azure.</p><br/>
         </>
       </Card>
     }
