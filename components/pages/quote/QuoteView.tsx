5d9881a0847383a917c359ae7afcfbd732b18b1d
Krystian Bajno <krystian.bajno@gmail.com>
2023-11-13 04:42:33 +0100

    instr

--- components/pages/quote/QuoteView.tsx
+++ components/pages/quote/QuoteView.tsx
@@ -19,18 +19,21 @@ const QuoteView = ({formik}) => {
                             <a href="mailto:info@baycode.eu">info@baycode.eu</a>
                         </div>
                         <div>
-                            <p>Feel free to use the contact form to reach me instantly.</p><br/>
-                            <h3>What information do I need to respond back?</h3><br/>
-                            <p>To facilitate communication, please provide your email address. If you have a preference for an alternative method such as XMPP, kindly include it in the Full Name field along with a placeholder dummy email address. This field is mandatory to ensure you don't overlook it and receive a prompt response. Your cooperation is appreciated!</p><br/>
-                            <p>Please provide your email address for communication. If you prefer an alternative method (XMPP etc.), enter it in the Full Name field along with a placeholder dummie email address. This field is mandatory to ensure you don't forget to fill it, and receive a response. Thank you!</p><br/>
-                            <h3>How does it work?</h3><br/>
-                            <p>Your message is random-key encrypted using AES-256, and sent securely through a Vercel cloud function to a Telegram bot. The bot stores the encrypted message alongside with a public-key RSA encrypted symmetric key in a chat room, and notifies me. I retrieve and decrypt the symmetric key using my private key and decrypt the message using symmetric key. The communication is fully secure, the data encrypted, and I have no way to determine who you are if you do not provide me with valid contact information.</p><br/>
-                            <h3>Is my <b>encrypted</b> contact information stored anywhere else than in a chat room?</h3><br/>
-                            <p>It is not, and I do not send it to no one else than private Telegram chat room without your permission. Your data is on Telegram servers, and remains fully encrypted with the symmetric key. Thank you for reaching out! :)</p><br/>
-                            <h3>If the data is unreadable, then what happens when the private key is lost?</h3><br/>
-                            <p>That would make all previous messages unreadable, just like a crypto wallet unaccessible. The data is mangled ones and zeroes with no way of recovering it, and Telegram is just an interchangeable communications channel.</p><br/>
-                            <h3>How does this website work?</h3><br/>
-                            <p>The website is serverless too, like the cloud function. It dies once in a while and reboots itself from Vercel cloud storage when someone opens it. No permanent hosting, and no visitors logging information by me.</p>
+                        <p>Feel free to use the contact form to reach me out instantly.</p><br/>
+                        <h3>Information Needed:</h3><br/>
+                        <p>Please provide your email address. If using an alternative like XMPP, include the contact in the Full Name field with a dummy email for a prompt response. I made this field required so you do not forget to fill in the information I will use to contact you back.</p><br/>
+                        <h3>How it Works:</h3><br/>
+                        <p>Your message is AES-256 CBC encrypted using a randomly generated symmetric key, and sent securely through Vercel serverless cloud function to a Telegram bot. The bot stores the encrypted message along with public key RSA encrypted symmetric key and notifies me. I receive the message as soon as you click the submit button. I decrypt the key using my offline private key, and next decrypt the message using the symmetric key.</p><br/>
+                        <h3>Data Storage:</h3><br/>
+                        <p>Your encrypted contact info is only in a private Telegram chat room. It's not sent elsewhere without your permission.</p><br/>
+                        <h3>Key Loss Impact:</h3><br/>
+                        <p>If the private key is lost, previous messages become unreadable, like an inaccessible crypto wallet. The communication through this unconventional form is very confidential.</p><br/>
+                        <h3>Security Measures:</h3><br/>
+                        <p>A breach of the cloud or Telegram account won't expose data. Attackers get only public key or mangled data, the private key is being stored safely offline. There is no confidential information stored on this website, nor accessible from cloud provider.</p><br/>
+                        <h3>Website Functionality:</h3><br/>
+                        <p>The serverless website, like the cloud function, reboots from Vercel storage when accessed. No permanent hosting or visitor logging.</p><br/>
+                        <p>Thanks for reaching out! :) </p><br/>
+                        <p><b>Now that you have all this information, feel free to try the contact form!</b></p>
                         </div>
                     </div>
                     <div className={[styles.form, styles.section].join(" ")}>
