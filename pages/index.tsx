79d5b12f92c909dd493e7d389cd84c9eb12963a4
Krystian Bajno <krystian.bajno@gmail.com>
2023-11-14 20:18:41 +0100

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home(props) {
     <>
       <Head>
         <title>Baycode - your business, our expertise - custom software, penetration tests</title>
-        <meta name="description" content="Our mission is to help organizations to grow their business through expert software engineering services and secure their systems to protect their data from cyber threats. We specialize in creating custom software solutions that meet your unique needs and ensure your systems are robust, reliable, and secure." />
+        <meta name="description" content="My name is Krystian Bajno, I'm a software developer and cyber security specialist (appsec, netsec). This little space on the web is where technology meets personality, and I'm thrilled to have you here. My domain covers full-stack software engineering as well as penetration tests and implementing S-SDLC programmes." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="favicon.svg" />
         <meta property="og:title" content="Baycode - your business, our expertise - custom software, penetration tests" />
@@ -17,7 +17,7 @@ export default function Home(props) {
         <meta property="og:image" content={ogimage.src} />
         <meta property="og:url" content="https://baycode.eu" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:description" content="Our mission is to help organizations to grow their business through expert software engineering services and secure their systems to protect their data from cyber threats. We specialize in creating custom software solutions that meet your unique needs and ensure your systems are robust, reliable, and secure." />
+        <meta property="og:description" content="My name is Krystian Bajno, I'm a software developer and cyber security specialist (appsec, netsec). This little space on the web is where technology meets personality, and I'm thrilled to have you here. My domain covers full-stack software engineering as well as penetration tests and implementing S-SDLC programmes." />
         <meta property="og:site_name" content="Baycode" />
         <meta name="twitter:image:alt" content="Baycode Logo" />
       </Head>
